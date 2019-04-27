e6de912201b624006d10de80723250c81abfa91a
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-04-27 14:14:07 +0100

    testing

--- II.write-tests/01-greet-people/greet-people.js
+++ II.write-tests/01-greet-people/greet-people.js
@@ -1,18 +1,24 @@
 function greetPeople(people) {
   var greeting = "Hello ";
 
-  people.forEach(function(person) {
-    greeting = greeting + person;
+  people.forEach(function(person, index) {
+    var lastElement = index === people.length - 1;
+    if (lastElement) {
+      greeting = greeting + "" + person;
+    } else {
+      greeting = greeting + "" + person + ", ";
+    }
   });
 
   return greeting;
 }
-
+//Last elements index  = Length - 1
 module.exports = greetPeople;
 
 /*
   Let's trace this piece of code - what is the value of result with this input
-
-  var mentors = ['Irina', 'Ashleigh', 'Etza'];
-  var result = greetPeople(mentors)
+var mentors = ["Irina", "Ashleigh", "Etza"];
+console.log(mentors.length);
+var result = greetPeople(mentors);
+  
 */
--- II.write-tests/01-greet-people/greet-people.test.js
+++ II.write-tests/01-greet-people/greet-people.test.js
@@ -1,5 +1,9 @@
+var greetPeople = require("./greet-people.js");
 test("print list of names prefixed with Hello", function() {
   // Arrange
+  var mentors = ["Irina", "Ashleigh", "Etza"];
   // Act
+  var result = greetPeople(mentors);
   // Assert
+  expect(result).toBe("Hello Irina, Ashleigh, Etza");
 });
